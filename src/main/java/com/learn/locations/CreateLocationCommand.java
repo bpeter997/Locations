4742cdc88b7774c0c3507c8458fc0edb487a287c
Bodnar Peter <bodnar.peter97@gmail.com>
2022-12-18 00:20:54 +0100

    update, create, delete location

---
+++ src/main/java/com/learn/locations/CreateLocationCommand.java
@@ -0,0 +1,16 @@
+package com.learn.locations;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLocationCommand {
+  private String name;
+  private double lat;
+  private double lon;
+}
--- src/main/java/com/learn/locations/LocationController.java
+++ src/main/java/com/learn/locations/LocationController.java
@@ -2,8 +2,12 @@ package com.learn.locations;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +34,22 @@ public class LocationController {
   @GetMapping("/{id}")
   public List<LocationDto> getLocationById(@PathVariable("id") long id) {
     return locationService.getLocationById(id);
+  }
+
+  @PostMapping
+  public LocationDto createLocation(@RequestBody CreateLocationCommand locationCommand) {
+    return locationService.createLocation(locationCommand);
+  }
+
+  @PutMapping("/{id}")
+  public LocationDto updateLocation(
+      @PathVariable("id") long id, @RequestBody UpdateLocationCommand locationCommand)
+      throws Exception {
+    return locationService.updateLocation(id, locationCommand);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteLocation(@PathVariable("id") long id) throws Exception {
+    locationService.deleteLocation(id);
   }
 }
